2db551d27a47fa6e9e01a2a048c9f8402751bdb6
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-07-25 02:27:50 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+function handleError(){
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+}
+
+btn.addEventListener('click', handleError);
